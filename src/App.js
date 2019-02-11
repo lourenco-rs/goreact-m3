114748cb8f25938d36b6e0a26673e4bc85087fe5
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-02-11 14:29:34 -0200

    Aula 7 - Reactotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import TodoList from './TodoList';
-
+import './config/reactotron';
 import store from './store';
 
+import TodoList from './TodoList';
+
+console.tron.log('testando console do tron');
 const App = () => (
   <Provider store={store}>
     <TodoList />
